5e24ee26df3ffed14a359d34d3fc84a53e0e3636
farnaz <farnaz.rashidi@gmail.com>
2017-05-08 21:19:18 -0700

    Create the request URL

--- download_avatar.js
+++ download_avatar.js
@@ -1,3 +1,13 @@
 var request = require('request');
+var GITHUB_USER = "Phlegz";
+var GITHUB_TOKEN = "dba0c4f050a8693a7a027ddfe6fc088aab009ae3";
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});

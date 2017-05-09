e735ca6b5a7b7cb9dd2481bbb5b035fe81f7745d
farnaz <farnaz.rashidi@gmail.com>
2017-05-08 23:52:56 -0700

    Make an http request to the api URL and log the response body

--- download_avatar.js
+++ download_avatar.js
@@ -2,12 +2,25 @@ var request = require('request');
 var GITHUB_USER = "Phlegz";
 var GITHUB_TOKEN = "dba0c4f050a8693a7a027ddfe6fc088aab009ae3";
 
-
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+
+  //HTTP Headers, such as User-Agent, can be set in the options object. Here, we call the github API. This requires a custom User-Agent header as well as https.
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'GitHub Avatar Downloader - Student Project'
+    }
+  };
+  request(options, cb);
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var info = JSON.parse(body);
+    console.log(info);
+  }
+}
+
+getRepoContributors("jquery", "jquery", callback);
